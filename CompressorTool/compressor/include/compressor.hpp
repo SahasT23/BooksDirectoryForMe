af8da8ab690fcdbcb202bed61083b4c2f4bdfcb1
SahasT23 <sahas.st01@gmail.com>
2025-07-24 07:47:25 +0100

    removed compression tool. (Not mine, AI generated)

--- CompressorTool/compressor/include/compressor.hpp
+++
@@ -1,8 +0,0 @@
-// compressor.hpp
-#pragma once
-#include <string>
-
-namespace compression {
-    void compress_file(const std::string& input, const std::string& output);
-    void decompress_file(const std::string& input, const std::string& output);
-}
